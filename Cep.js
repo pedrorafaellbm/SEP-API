f314e03174277d69687cf94cdb547cefd29a0542
PEDRO RAFAEL LIMA BEZERRA DE MEDEIROS <pedromedeiros0978@digitalcollege.internal>
2025-06-24 22:11:08 -0300

    fim da aula entre aspas de API

--- Cep.js
+++ Cep.js
@@ -10,9 +10,9 @@ fetch(uri)
   .then(json => {
     console.log(json)
 
-    document.getElementById('Logradouro').value = json.adress
-    document.getElementById('Bairro').value = json.adress
-    document.getElementById('UF').value = json.adress
+    document.getElementById('Logradouro').value = json.address
+    document.getElementById('Bairro').value = json.district
+    document.getElementById('UF').value = json.state
 
 })
 }
