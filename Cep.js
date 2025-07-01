cfe3897c4b9671e3994c39ece2b4624870381971
PEDRO RAFAEL LIMA BEZERRA DE MEDEIROS <pedromedeiros0978@digitalcollege.internal>
2025-07-01 20:09:47 -0300

    organizando api

--- Cep.js
+++ Cep.js
@@ -1,18 +1,41 @@
 const consultarCep = () => {
     console.log('chamou a API')
-const cep = document.getElementById('cep').value
+    const cep = document.getElementById('cep').value
 
-let uri = `https://cep.awesomeapi.com.br/json/${cep}`
+    let uri = `https://cep.awesomeapi.com.br/json/${cep}`
 
-console.log(`URI: ${uri}`)
-fetch(uri)
-  .then(response => response.json())
-  .then(json => {
-    console.log(json)
+    console.log(`URI: ${uri}`)
+    fetch(uri)
+        .then(response => response.json())
+        .then(json => {
+            console.log(json)
 
-    document.getElementById('Logradouro').value = json.address
-    document.getElementById('Bairro').value = json.district
-    document.getElementById('UF').value = json.state
+            document.getElementById('Logradouro').value = json.address
+            document.getElementById('Bairro').value = json.district
+            document.getElementById('UF').value = json.state
+
+        })
+}
+
+const fetchEstados = () => {
+    let uri = `https://servicodados.ibge.gov.br/api/v1/localidades/estados`
+
+    console.log(`URI: ${uri}`)
+
+    fetch(uri)
+        .then(response => response.json())
+        .then(json => {
+            console.log(json)
+
+    let options = ''
+
+    data.forEach(estado => {
+        options = options + `<opition value="${estado.sigla}">${estado.nome}</opition>`
+    })
 
 })
+
+    document.getElementById('UF').innerHTML = Options
+
+
 }
